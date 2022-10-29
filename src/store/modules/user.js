ecbce05ea0c6e81e2c0ce5ad15804beac089d2ca
jiu-Li <2920460143@qq.com>
2022-10-29 11:02:24 +0800

    封装获取用户资料的action并共享用户状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,13 @@
 import {getToken, setToken, removeToken, } from '@/utils/auth'
-import { login, } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
+import { start } from 'nprogress'
 // import { resetRouter} from '@/router'
 
 //状态
 // 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
 const state = {
   token: getToken(), //设置token 为共享 // 设置token初始状态   token持久化 => 放到缓存中
-  userInfo:{} //这里为什么不写null?
+  userInfo:{} //这里为什么不写null? 空对象 应为在getters 里面应用userinfo 的变量, 如果设置null 则会引起异常报错
 }
 
 //修改状态
@@ -17,12 +18,23 @@ const mutations = {
     // 需要将 vuex 中的数据同步 到缓存
     setToken(token)
   },
-
   //删除缓存
   removeToken(state){
     state.token = null // 设置vuex中的token 为null
     removeToken()  //同步删除缓存中的 token
+  },
+
+  // 设置用户信息
+  setUserInfo(state, result){
+    //更新对象
+    state.userInfo = result  //这样是响应式
+    // state.userInfo = {...result} // 这样也是响应式 属于浅拷贝
+  },
+  // 删除用户信息
+  removeUserInfo(state){
+    state.userInfo = {} 
   }
+
 }
 
 // 执行异步
@@ -38,6 +50,12 @@ const actions = {
     // 现在有用户token
     // actions 修改state 必须通过mutations
     context.commit("setToken", result) //设置token
+  },
+
+  async getUserInfo (context) {
+    const result =  await getUserInfo()
+    context.commit("getUserInfo",result) //提交到 mutations
+    return result  // 这里为什么要返回 为后面做权限的时候 埋下伏笔
   }
 }
 
