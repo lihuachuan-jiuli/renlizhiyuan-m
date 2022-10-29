dd1f90fcef790e24dbbbf7b9191566d06c8832fc
jiu-Li <2920460143@qq.com>
2022-10-29 19:22:06 +0800

    完成其他模块的页面和路由的快速搭建

---
+++ src/router/modules/salarys.js
@@ -0,0 +1,17 @@
+// 导出员工的路由规则
+import Layout from "@/layout"
+export default {
+  // 路由规则
+  path: '/salarys', //路由地址
+  name: "salarys", //给模块的一级路由加一个name 属性
+  component: Layout,
+  children:[{
+    // 二级路由的path 什么的不写的时候, 此时它表示二级路由的默认路由
+    path: '',
+    component: ()=> import('@/views/salarys'),
+    // 路由元信息 , 其实就是储存数据的对象, 我们可以在里面放一些信息
+    meta: { 
+      title: "工资"
+     }
+  }]
+}
