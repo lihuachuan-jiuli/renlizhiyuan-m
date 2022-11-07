d826cf5fad43babc81dad5979c3e45bae9760003
jiu-Li <2920460143@qq.com>
2022-11-07 09:08:14 +0800

    实现动态主题设置

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,16 @@
 import PageToods from './pageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
+import ScreenFull from './ScreeFull'
+import ThemePicker from './ThemePicker'
 export default {
   install(Vue) {
     // 组件的注册
     Vue.component('PageTools', PageToods)
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
     Vue.component('ImageUpload', ImageUpload) // 注册图片上传组件
+    Vue.component('ScreenFull', ScreenFull)
+    Vue.component('ThemePicker', ThemePicker)
   }
 }
 
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -6,6 +6,10 @@
       <span class="breadBtn">体验版</span>
     </div>
     <div class="right-menu">
+      <!-- 放置全屏插件 -->
+      <ScreenFull class="right-menu-item" />
+      <!-- 放置切换主题组件 -->
+      <ThemePicker class="right-menu-item" />
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
 
@@ -126,7 +130,7 @@ export default {
       height: 100%;
       font-size: 18px;
       color: #5a5e66;
-      vertical-align: text-bottom;
+      vertical-align:middle;
 
       &.hover-effect {
         cursor: pointer;
